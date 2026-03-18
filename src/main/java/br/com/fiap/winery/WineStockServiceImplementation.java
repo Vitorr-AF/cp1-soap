fa1eb3417956ce29a78d2efc4b521b2a2c33fbf0
Vitorr-AF <vitoraugustofranca7@gmail.com>
2026-03-18 09:02:05 -0300

    Projeto criado

---
+++ src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
@@ -0,0 +1,17 @@
+package br.com.fiap.winery;
+
+import jakarta.jws.WebService;
+
+@WebService(endpointInterface = "br.com.fiap.winery.WineStockService")
+public class WineStockServiceImplementation implements WineStockService {
+
+    @Override
+    public String getMenu(){
+        return "Vinho tinto, Vinho branco, Vinho rosé, Vinho espumante";
+    }
+
+    @Override
+    public String placeOrder(String nome, int quantidade){
+        return "Pedido de "+quantidade+ " " + nome+ " confirmado!";
+    }
+}
